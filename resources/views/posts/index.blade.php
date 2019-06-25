b1883d1177f42204300cc488c10f046451e80a55
Callens Ben <callens.ben@github.com>
2019-06-25 12:17:22 +0200

    lesson 31-32

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,32 @@
     <div class="card card-default">
         <div class="card-header">Posts</div>
         <div class="card-body">
-
+            <table class="table">
+                <thead>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th></th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>
+                                <img src="{{ asset('/storage/'.$post->image) }}" width="120px" height="80px" alt="">
+                            </td>
+                            <td>
+                                {{ $post->title }}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-danger ">Trash</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
